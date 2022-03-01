8a97c0521b32aa17f9f739b6e9fe0a6d16313031
Supreeti Kumari <78208708+SupreetiKumari@users.noreply.github.com>
2022-03-01 21:30:21 +0530

    Update library.cpp

--- subtask3/library.cpp
+++ subtask3/library.cpp
@@ -1,4 +1,20 @@
+#include <vector>
+#include <math.h>
+#include <string>
+#include <fstream>
+using namespace std;
+
 typedef struct{
     int label;
     float prob;
 }pred_t;
+
+vector<vector<float>> weight1;
+vector<vector<float>> weight2;
+vector<vector<float>> weight3;
+vector<vector<float>> weight4;
+vector<vector<float>> bias1;
+vector<vector<float>> bias1;
+vector<vector<float>> bias1;
+vector<vector<float>> bias1;
+
